3187e13d7f297a19dc85ad8df32807fb19ea3d75
= <=>
2013-08-12 22:36:34 +0800

    Support input path

--- deleteNotMoveFile.py
+++ deleteNotMoveFile.py
@@ -1,7 +1,13 @@
-import os;
+import os,sys;
+
+if(len(sys.argv) >= 2) :
+  dirPath = sys.argv[1];
+else:
+  dirPath = "~/share/Video";
+
 
 fileGrep = '-not -iname "*.avi" -not -iname "*.mkv" -not -iname "*.rmvb" -not -iname "*.mp4" -not -iname "*.3gp" -not -iname "*.flv" -not -iname "*.mpg" -not -iname "*.mgeg" -not -iname "*.swf" -not -iname "*.wmv"';
 
-command = 'sudo find ~/share/movie/Video -type f -size -100000 ' + fileGrep + ' -exec rm -fv {} \;';
+command = 'sudo find ' + dirPath + ' -type f -size -100000 ' + fileGrep + ' -exec rm -fv {} \;';
 
 os.system(command);
