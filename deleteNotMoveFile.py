5e3360cecdf4bfec781fc3f7272a6a3f2907837e
= <=>
2016-06-08 10:00:18 +0800

    Leave rm file

--- deleteNotMoveFile.py
+++ deleteNotMoveFile.py
@@ -6,7 +6,7 @@ else:
   dirPath = "~/share/Video";
 
 
-fileGrep = '-not -iname "*.avi" -not -iname "*.mkv" -not -iname "*.rmvb" -not -iname "*.mp4" -not -iname "*.3gp" -not -iname "*.flv" -not -iname "*.mpg" -not -iname "*.mgeg" -not -iname "*.swf" -not -iname "*.wmv"';
+fileGrep = '-not -iname "*.avi" -not -iname "*.mkv" -not -iname "*.rmvb" -not -iname "*.mp4" -not -iname "*.3gp" -not -iname "*.flv" -not -iname "*.mpg" -not -iname "*.mgeg" -not -iname "*.swf" -not -iname "*.wmv" -iname "*.rm"';
 
 command = 'sudo find ' + dirPath + ' -type f -size -100000 ' + fileGrep + ' -exec rm -fv {} \;';
 
